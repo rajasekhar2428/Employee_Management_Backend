01f5dfd2519011f51a93685ef3ca675f7a4d74e3
rajasekhar2428 <100747394+rajasekhar2428@users.noreply.github.com>
2024-03-28 18:57:00 +0530

    Update UserRepository.java

--- src/main/java/com/devBackend/FullstackReactbackend/repository/UserRepository.java
+++ src/main/java/com/devBackend/FullstackReactbackend/repository/UserRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devBackend.FullstackReactbackend.Model.User;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	List<User> findByDepartment(String department);
 }
